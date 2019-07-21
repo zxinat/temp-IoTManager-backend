c5896d0109b17b036a74f463d72b9ed24f13a7ca
JackySong59 <1055335350@qq.com>
2019-07-21 16:17:25 +0800

    Write CORS Ip Address to Appsettings.json

--- IoTManager.Utility/Constant.cs
+++ IoTManager.Utility/Constant.cs
@@ -18,7 +18,7 @@ namespace IoTManager.Utility
             //return
                 //"mongodb://iotmangermongodb:1BiabpxKh7xotBoShVPkp8IvKaNvb2DoIqfZ9SfZ8tg8Cpwj2y5DKgdK8gVDZZelCfPonWfVJge66y3UjWS6KA==@iotmangermongodb.documents.azure.cn:10255/?ssl=true&replicaSet=globaldb";
                 return
-                    "mongodb://shudev2:Etp13A3NROECpQeJ1GjTbkj7OqHfoukak17BwiMgcjw6g2ap5PPZsfraINEVJ1G34UtR2MHUJCTufvhAz2uwLQ==@shudev2.documents.azure.cn:10255/?ssl=true&replicaSet=globaldb";
+                    "mongodb://localhost:27017";
         }
 
         public static String getDateFormatString()
